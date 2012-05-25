169c8b39de58ecefc7fd35c5c05e34f4193b56cf
mitchellshelton <shelton.5@osu.edu>
2012-05-25 08:55:12 -0400

    Spelling correction on about page.

--- content/about.php
+++ content/about.php
@@ -6,7 +6,7 @@
     Keptosh Media is Game Development and Consultation company. 
     </p>
     <p>
-    We are focused on creating innovative cross platform games at an affordable price. We are a small part of a great revolution in gaming where big publishers and big studios are quickly being over taken by smaller companies and individual game developers. We hope to be a bigger part of this diruptive market in the video games industry in the near future.
+    We are focused on creating innovative cross platform games at an affordable price. We are a small part of a great revolution in gaming where big publishers and big studios are quickly being over taken by smaller companies and individual game developers. We hope to be a bigger part of this disruptive market in the video games industry in the near future.
     </p>
     <p>
     Keptosh Media is named after the first game created by our founder <a href="http://www.mitchellshelton.com/" title="Visit Mitchell Shelton's personal homepage.">Mitchell Shelton</a>. <a href="/projects/keptosh" title="View the Keptosh project page.">Keptosh</a> is an adventure game that was released in 2003 for the Microsoft Windows platform using the <a href="http://www.adventuregamestudio.co.uk/" title="Visit the AGS website.">AGS</a> game engine.    </p>
